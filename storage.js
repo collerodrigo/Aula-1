310b099a47898c6e8ad3eba1e8b8433b8e1716bf
COLLE <colle681@gmail.com>
2024-12-16 22:39:32 +0100

    js para salvar no storage

---
+++ storage.js
@@ -0,0 +1,7 @@
+function save() {
+    localStorage.name = document.getElementById("name").value;
+    localStorage.lastName = document.getElementById("lastName").value;
+    localStorage.email = document.getElementById("email").value;
+    localStorage.age = document.getElementById("age").value;
+    localStorage.gender = document.getElementById("gender").value;
+}
