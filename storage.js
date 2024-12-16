f126c0662c664b3da7811fc3de8571b7df024be7
COLLE <colle681@gmail.com>
2024-12-16 22:52:02 +0100

    function show

--- storage.js
+++ storage.js
@@ -5,3 +5,11 @@ function save() {
     localStorage.age = document.getElementById("age").value;
     localStorage.gender = document.getElementById("gender").value;
 }
+
+function show() {
+    document.getElementById("name").value = localStorage.name;
+    document.getElementById("lastName").value = localStorage.lastName;
+    document.getElementById("email").value = localStorage.email;
+    document.getElementById("age").value = localStorage.age;
+    document.getElementById("gender").value = localStorage.gender;
+}
